b8fbb59ade1f1c5c7d2f59327e53ec9abf882e2a
Jorge Pizarro <jorge.pizarro19@outlook.es>
2022-07-04 14:00:43 -0500

    Añadí algunos comentarios

--- Curso Python/POO/InitSelf.py
+++ Curso Python/POO/InitSelf.py
@@ -2,7 +2,7 @@ class FabricaDeCelulares():
     marca = "OnePLus"
     
     def MadeSamsung(self): #Se prefiere el uso de letras minusculas para los métodos
-        self.marca = "Samsung"
+        self.marca = "Samsung" #El uso del metodo "self" es para hacer global la la variable y su cambio
         
 telefono = FabricaDeCelulares()
 
