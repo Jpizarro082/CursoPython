10640755f8b02ca5f12706f71ee43795414eca6d
Jorge Pizarro <jorge.pizarro19@outlook.es>
2022-07-04 13:34:56 -0500

    Escribi codigo

---
+++ Curso Python/POO/MetodoAtributos.py
@@ -0,0 +1,21 @@
+class FabricadeTelefonos():
+    marca = "OnePlus"
+    color = "Midight Blue"
+    memoria_ram = 8
+    almacenamiento = 256
+    
+    def llamar(self, mensaje):
+        return mensaje
+    
+telefono = FabricadeTelefonos()
+    
+print(telefono.marca)
+print(telefono.color)
+print(telefono.memoria_ram)
+print(telefono.almacenamiento)
+
+print(telefono.llamar("Llamando a Aurora"))
+    
+    
+    
+    
